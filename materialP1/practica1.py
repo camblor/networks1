a23612b6ac121b52e76c35eaf0d2a476bd29addb
camblor <alfonsocamblor@gmail.com>
2019-09-22 16:40:20 +0000

    second upload

--- materialP1/practica1.py
+++ materialP1/practica1.py
@@ -9,6 +9,7 @@
 '''
 
 from rc1_pcap import *
+import datetime
 import sys
 import binascii
 import signal
@@ -16,6 +17,7 @@ import argparse
 from argparse import RawTextHelpFormatter
 import time
 import logging
+import binascii
 
 ETH_FRAME_MAX = 1514
 PROMISC = 1
@@ -32,14 +34,28 @@ def signal_handler(nsignal,frame):
 		
 
 def procesa_paquete(us,header,data):
-	global num_paquete, pdumper
+	global num_paquete, pdumper, num_bytes
+	dataheader = pcap_pkthdr()
 	logging.info('Nuevo paquete de {} bytes capturado a las {}.{}'.format(header.len,header.ts.tv_sec,header.ts.tv_sec))
 	num_paquete += 1
 	#TODO imprimir los N primeros bytes
+	dataheader.len = header.len
+	dataheader.caplen = header.caplen
+	firstbytes = str(data[:num_bytes])
+	print(bytearray(str(data[:num_bytes]), 'utf-8').decode())
+	print('Primeros ' + str(num_bytes) + ' bytes: ' + firstbytes)
+	
+	#dataheader.caplen = header[0].caplen
+	#dataheader.ts = timeval(header[0].tv_sec,header[0].tv_usec)
 	#Escribir el tráfico al fichero de captura con el offset temporal
 	
+
+	
+
+    
+	
 if __name__ == "__main__":
-	global pdumper,args,handle
+	global pdumper,args,handle, num_bytes
 	parser = argparse.ArgumentParser(description='Captura tráfico de una interfaz ( o lee de fichero) y muestra la longitud y timestamp de los 50 primeros paquetes',
 	formatter_class=RawTextHelpFormatter)
 	parser.add_argument('--file', dest='tracefile', default=False,help='Fichero pcap a abrir')
@@ -60,6 +76,7 @@ if __name__ == "__main__":
 
 	signal.signal(signal.SIGINT, signal_handler)
 
+	num_bytes = 10
 	errbuf = bytearray()
 	handle = None
 	pdumper = None
@@ -71,29 +88,43 @@ if __name__ == "__main__":
 	'''
 	-SITUACION --itf
 	-CAPTURA DE INTERFAZ ESPECIFICADA
-	
-	if True is True:
+	'''
+	if args.interface:
 		#Apertura de la interfaz especificada para captura o la traza (Importante superusuario)
 		handle = pcap_open_live(args.interface, BUFSIZ,0, 100, errbuf)
 
 		#Apertura de un dumper para volcar el tráfico (si se ha especificado interfaz)
 		descr2 = pcap_open_dead(DLT_EN10MB,1514)
-		pdumper = pcap_dump_open(descr2,'salida.pcap')
+		date  = datetime.datetime.now()
+		traza_name = 'captura.' + str(args.interface) + '.' + str(date.year) + '_' + str(date.month) + '_' + str(date.day) + '.pcap'
+		pdumper = pcap_dump_open(descr2,traza_name)
 
 		#Uso del dump
 		pcap_dump(pdumper,h,pkt_data)
 		
+	
+
+	'''
+	-SITUACION --file
+	-LECTURA DE LA TRAZA ESPECIFICADA
+	 El programa debe almacenar los paquetes capturados enteros en una traza con nombre captura.nombreitf.FECHA.pcap 
+	 (donde FECHA será el tiempo actual UNIX en segundos y nombreitf el nombre de la interfaz especificada).
 	'''
 
-	print("Entra en el handle con: " + args.tracefile)
+	if args.tracefile:
+		print("Abriendo archivo " + str(args.tracefile) + "...")
+		handle = pcap_open_offline(args.tracefile, errbuf)
+		print("Apertura completada...")
 
-	handle = pcap_open_offline(args.tracefile, errbuf)
 
-	print("HOLALA" + str(args.tracefile))
+	'''
+	-SITUACION --nbytes
+	-IMPRESION POR PANTALLA DE LOS PRIMEROS nbytes DE CADA PAQUETE
+	'''
 	
 	
 	#Comienzo del bucle de captura
-	if handle is not None:	
+	if handle:	
 		print("-- Entrando en bucle de captura --\n")
 		ret = pcap_loop(handle,50,procesa_paquete,None)
 		print("-- Saliendo del bucle de captura --\n")
