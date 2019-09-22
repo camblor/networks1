98f6a49ee9a81c2e177e22a3720bc8134f126b26
camblor <alfonsocamblor@gmail.com>
2019-09-22 17:22:25 +0000

    Completed

--- materialP1/practica1.py
+++ materialP1/practica1.py
@@ -20,6 +20,7 @@ import logging
 import binascii
 
 ETH_FRAME_MAX = 1514
+PAQUETES_TOTAL = 5
 PROMISC = 1
 NO_PROMISC = 0
 TO_MS = 10
@@ -34,20 +35,24 @@ def signal_handler(nsignal,frame):
 		
 
 def procesa_paquete(us,header,data):
-	global num_paquete, pdumper, num_bytes
-	dataheader = pcap_pkthdr()
-	logging.info('Nuevo paquete de {} bytes capturado a las {}.{}'.format(header.len,header.ts.tv_sec,header.ts.tv_sec))
+	global num_paquete, pdumper, num_bytes, debug
+	databytes = []
+	logging.info('Nuevo paquete de {} bytes capturado a las {}.{}'.format(header.len,header.ts.tv_sec,header.ts.tv_usec))
 	num_paquete += 1
-	#TODO imprimir los N primeros bytes
-	dataheader.len = header.len
-	dataheader.caplen = header.caplen
-	firstbytes = str(data[:num_bytes])
-	print(bytearray(str(data[:num_bytes]), 'utf-8').decode())
-	print('Primeros ' + str(num_bytes) + ' bytes: ' + firstbytes)
 	
-	#dataheader.caplen = header[0].caplen
-	#dataheader.ts = timeval(header[0].tv_sec,header[0].tv_usec)
+	#Impresion de los N primeros bytes
+	#Cambiamos el formato de impresion a hexadecimal con 2 digitos por byte
+	for value in data[:num_bytes]:
+		databytes.append("{:02x}".format(value))
+	print("---------------------------")
+
+	
+	print('Primeros ' + str(num_bytes) + ' bytes: ' + str(databytes))
+	print("---------------------------\n")
+
 	#Escribir el tráfico al fichero de captura con el offset temporal
+	if pdumper:
+		pcap_dump(pdumper,header,data)
 	
 
 	
@@ -55,7 +60,8 @@ def procesa_paquete(us,header,data):
     
 	
 if __name__ == "__main__":
-	global pdumper,args,handle, num_bytes
+	global pdumper,args,handle, num_bytes, debug
+	debug = False
 	parser = argparse.ArgumentParser(description='Captura tráfico de una interfaz ( o lee de fichero) y muestra la longitud y timestamp de los 50 primeros paquetes',
 	formatter_class=RawTextHelpFormatter)
 	parser.add_argument('--file', dest='tracefile', default=False,help='Fichero pcap a abrir')
@@ -98,9 +104,7 @@ if __name__ == "__main__":
 		date  = datetime.datetime.now()
 		traza_name = 'captura.' + str(args.interface) + '.' + str(date.year) + '_' + str(date.month) + '_' + str(date.day) + '.pcap'
 		pdumper = pcap_dump_open(descr2,traza_name)
-
-		#Uso del dump
-		pcap_dump(pdumper,h,pkt_data)
+		
 		
 	
 
@@ -112,22 +116,38 @@ if __name__ == "__main__":
 	'''
 
 	if args.tracefile:
-		print("Abriendo archivo " + str(args.tracefile) + "...")
-		handle = pcap_open_offline(args.tracefile, errbuf)
-		print("Apertura completada...")
-
+		if(debug == True):
+			print("Abriendo archivo " + str(args.tracefile) + "...")
+			handle = pcap_open_offline(args.tracefile, errbuf)
+			print("Apertura completada...")
+		else:
+			handle = pcap_open_offline(args.tracefile, errbuf)
 
 	'''
 	-SITUACION --nbytes
 	-IMPRESION POR PANTALLA DE LOS PRIMEROS nbytes DE CADA PAQUETE
 	'''
+
+	if args.nbytes:
+		num_bytes = args.nbytes
 	
+	'''
+	-SITUACION --debug
+	-IMPRESION DE MENSAJES DEPURACION
+	'''
+	if args.debug:
+		debug = True
 	
 	#Comienzo del bucle de captura
-	if handle:	
-		print("-- Entrando en bucle de captura --\n")
-		ret = pcap_loop(handle,50,procesa_paquete,None)
-		print("-- Saliendo del bucle de captura --\n")
+	if handle:
+		if debug == True:
+			print("-- Entrando en bucle de captura --\n")
+			ret = pcap_loop(handle,PAQUETES_TOTAL,procesa_paquete,None)
+			print("-- Saliendo del bucle de captura --\n")
+		else:
+			ret = pcap_loop(handle,PAQUETES_TOTAL,procesa_paquete,None)
+
+		pcap_close(handle)
 
 	#Casos posibles al ejecutar el bucle de captura (Pred: Error)
 	if ret == -1:
@@ -139,7 +159,7 @@ if __name__ == "__main__":
 	logging.info('{} paquetes procesados'.format(num_paquete))
 
 	#Cerrado del dump
-	if pdumper != None:
+	if pdumper:
 		pcap_dump_close(pdumper)	
 
 	
